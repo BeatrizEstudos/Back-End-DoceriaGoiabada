0a157592ff0f176fc5eb2ffba8671d0427ac156d
BeatrizEstudos <beatrizrochelly19@gmail.com>
2025-06-17 23:32:50 -0300

    Criação das rotas para Produto e Pedido

---
+++ src/routes/pedidoRoutes.js
@@ -0,0 +1,13 @@
+const router = express.Router();
+const PedidoController = require ("../controllers/pedidoController")
+
+
+router.get("/pedidos-geral", PedidoController.) //Rota privada só admin tem acesso
+router.get("/pedido-especifico/:id", PedidoController.)
+router.post("/aceita-pedido/:id", PedidoController.)
+router.post("/pedido-pronto/:id", PedidoController. )
+router.post("/finalizar-pedido/:id", PedidoController.)
+router.delete("/cancelar-pedido/:id", PedidoController.)
+
+module.exports = router
+
---
+++ src/routes/produtoRoutes.js
@@ -0,0 +1,9 @@
+const router = express.Router();
+const ProdutoController = require ("../controllers/produtoController")
+
+router.get("produtos-geral", ProdutoController.)
+router.post("adicionar-produto", ProdutoController.)
+router.put("atualizar-produto/:id", PedidoController.)
+router.delete("deletar-produto/:id", PedidoController.)
+
+module.exports = router
