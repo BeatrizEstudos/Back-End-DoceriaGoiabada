f78de5bb7c09068a2853da219bb8a2db4d805a3e
BeatrizEstudos <beatrizrochelly19@gmail.com>
2025-06-25 23:53:45 -0300

    adicionando middleware

---
+++ src/middleware/validacaoLojaEData.js
@@ -0,0 +1,25 @@
+const logger = require ("../utils/logger")
+
+const validacaoGeralPedido = (req, res, next) => {
+  const dataRegex = /^\d{2}-\d{2}-\d{4}$/;
+  const lojaId = req.headers["loja-id"]
+  const dataFiltro = req.params.data;
+
+  if (!lojaId) {
+    logger.info({
+      message: "LojaId incorreta ou não adicionada",
+      lojaId
+    })
+    return res.status(400).json({ message: "Para consultar todos os pedidos lojaId e necessário!" })
+  }
+
+  if (dataFiltro && !dataRegex.test(dataFiltro)) {
+    logger.info({
+      message: "Formato de data inválido na URL",
+      dataFiltro
+    });
+    return res.status(400).json({ message: "O formato da data na URL deve ser DD-MM-YYYY (ex: /pedidos-geral/24-06-2025)." });
+  }
+  next();
+}
+  module.exports = validacaoGeralPedido
