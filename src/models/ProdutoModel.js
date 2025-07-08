30421a9956f334fa09d2c523b43686555161799b
BeatrizEstudos <beatrizrochelly19@gmail.com>
2025-07-07 23:38:00 -0300

    Atualizando a model de produto

--- src/models/ProdutoModel.js
+++ src/models/ProdutoModel.js
@@ -1,13 +1,28 @@
 import mongoose from 'mongoose';
 
 const ProdutoSchema = new mongoose.Schema({
-  produtos: {
-    nome: String,
-    preco: Number,
-    ingredientes: String,
-    imagem: String
+  
+  loja: { 
+    type: String,
+    required: true
   },
-});
+  nome: {
+    type: String,
+    required: true 
+  },
+  preco: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  ingredientes: {
+    type: String
+  },
+  imagem: {
+    type: String
+  },
+  
+}, { timestamps: true }); 
 
 const Produto = mongoose.model('Produto', ProdutoSchema);
 export default Produto;
